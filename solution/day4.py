2702a9af1cd329dedb205478303d2cc0a8369ee6
Nathan Pannell <n8tewolf@gmail.com>
2022-12-05 01:23:44 -0800

    reordered into folders

---
+++ solution/day4.py
@@ -0,0 +1,14 @@
+import re
+
+def main():
+    input = '../input/day4.txt'
+    f = open(input, 'r')
+    total = 0
+    for line in [line.strip() for line in f]:
+        x0, x1, x2, x3 = [int(i) for i in re.split(',|-', line)]
+        total += [i for i in range(x0, x1 + 1) if i in range(x2, x3 + 1)] != [] 
+    print(total)
+    f.close()
+
+if __name__ == '__main__':
+    main()
