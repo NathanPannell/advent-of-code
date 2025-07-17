3d44001858863eeee767f213f31b08030b2e8eaa
Nathan Pannell <n8tewolf@gmail.com>
2025-07-16 21:34:02 -0700

    refactor: combine solutions for day 2

--- 2015/02/main.go
+++ 2015/02/main.go
@@ -7,41 +7,33 @@ import (
 	"strings"
 )
 
-func part1(input []string) int {
-	total := 0
+func solve(input []string) (int, int) {
+	wrappingPaperArea := 0
+	ribbonLength := 0
+
 	for _, dimensions := range input {
 		split := strings.Split(dimensions, "x")
-
 		l, _ := strconv.Atoi(split[0])
 		w, _ := strconv.Atoi(split[1])
 		h, _ := strconv.Atoi(split[2])
 
-		leastSide := l * w * h / max(l, w, h)
-		total += leastSide + 2*(l*w+w*h+l*h)
-	}
-	return total
-}
-
-func part2(input []string) int {
-	total := 0
-	for _, dimensions := range input {
-		split := strings.Split(dimensions, "x")
-
-		l, _ := strconv.Atoi(split[0])
-		w, _ := strconv.Atoi(split[1])
-		h, _ := strconv.Atoi(split[2])
-
+		totalSurfaceArea := 2 * (l*w + w*h + l*h)
+		smallestSideArea := l * w * h / max(l, w, h)
 		smallestPerimeter := 2 * (l + w + h - max(l, w, h))
-		volume := l * w * h
-		total += smallestPerimeter + volume
+		totalVolume := l * w * h
+
+		wrappingPaperArea += totalSurfaceArea + smallestSideArea
+		ribbonLength += smallestPerimeter + totalVolume
 	}
-	return total
+
+	return wrappingPaperArea, ribbonLength
 }
 
 func main() {
 	input := utils.ReadInput(2)
+	part1, part2 := solve(input)
 
 	fmt.Println("-- Day 02 --")
-	fmt.Printf("Part 1: %d\n", part1(input))
-	fmt.Printf("Part 2: %d\n", part2(input))
+	fmt.Printf("Part 1: %d\n", part1)
+	fmt.Printf("Part 2: %d\n", part2)
 }
