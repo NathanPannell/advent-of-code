244f01e26f784ad3519141e735a2e1ba634ae88f
Nathan Pannell <n8tewolf@gmail.com>
2025-07-18 20:09:47 -0700

    refactor: solved day 5 with loops since no regex support

--- 2015/05/main.go
+++ 2015/05/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"advent/utils"
 	"fmt"
+	"regexp"
 	//"github.com/glenn-brown/golang-pkg-pcre/src/pkg/pcre"
 )
 
@@ -10,25 +11,53 @@ func solve(input []string) (int, int) {
 	part1 := 0
 	part2 := 0
 
-	// Using a Perl-Compatible Regex to support backreferences `\1`
-	//threeVowels := pcre.MustCompile(`([aeiou].*){3}`, 0)
-	//repeatedChar := pcre.MustCompile(`(.)\1`, 0)
-	//bannedPair := pcre.MustCompile(`ab|cd|pq|xy`, 0)
-	//repeatedPair := pcre.MustCompile(`(..)\1`, 0)
-	//repeatedCharSeparated := pcre.MustCompile(`(.).\1`, 0)
+	threeVowels := regexp.MustCompile(`([aeiou].*){3}`)
+	bannedPair := regexp.MustCompile(`ab|cd|pq|xy`)
 
-	//for _, s := range input {
-	//	if threeVowels.MatcherString(s, 0).Matches() &&
-	//		repeatedChar.MatcherString(s, 0).Matches() &&
-	//		!bannedPair.MatcherString(s, 0).Matches() {
-	//		part1++
-	//	}
-	//
-	//	if repeatedPair.MatcherString(s, 0).Matches() &&
-	//		repeatedCharSeparated.MatcherString(s, 0).Matches() {
-	//		part2++
-	//	}
-	//}
+	// `(.)\1`
+	repeatedChar := func(s string) bool {
+		for i := 0; i < len(s)-1; i++ {
+			if s[i] == s[i+1] {
+				return true
+			}
+		}
+		return false
+	}
+
+	// `(.).\1`
+	repeatedCharSpaced := func(s string) bool {
+		for i := 0; i < len(s)-2; i++ {
+			if s[i] == s[i+2] {
+				return true
+			}
+		}
+		return false
+	}
+
+	// `(..).*\1`
+	pair := func(s string) bool {
+		for i := 0; i < len(s)-3; i++ {
+			for j := i + 2; j < len(s)-1; j++ {
+				if s[i:i+2] == s[j:j+2] {
+					return true
+				}
+			}
+		}
+		return false
+	}
+
+	for _, s := range input {
+		if threeVowels.MatchString(s) &&
+			repeatedChar(s) &&
+			!bannedPair.MatchString(s) {
+			part1++
+		}
+
+		if pair(s) &&
+			repeatedCharSpaced(s) {
+			part2++
+		}
+	}
 
 	return part1, part2
 }
